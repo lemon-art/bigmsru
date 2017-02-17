29511a4bd2c28ee2a028ff163a175577fc81d8c5
Oleg Kasatkin <oleg@lemon-art.ru>
2017-02-17 10:36:02 +0300

    start2

--- bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
+++ bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001487264195';
-$dateexpire = '001487274995';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:22:"C_R_2017-02-17_UAH_RUB";a:4:{s:11:"curFromRate";d:21.026599999999998;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
+$datecreate = '001487278887';
+$dateexpire = '001487289687';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:22:"C_R_2017-02-18_UAH_RUB";a:4:{s:11:"curFromRate";d:21.178699999999999;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
 return true;
 ?>
