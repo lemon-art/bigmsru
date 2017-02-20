524398ef67bcc44ce9c6706b85a8439a3efe7fd7
Oleg Kasatkin <oleg@lemon-art.ru>
2017-02-20 16:00:25 +0300

    updating catalog section2

---
+++ bitrix/stack_cache/MYSQL/currency_currency_lang/06/06e86a31a9acbf1f2b244a314b769dc6.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001487588372';
+$dateexpire = '001487599172';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:6:"RUB_ru";a:13:{s:8:"CURRENCY";s:3:"RUB";s:3:"LID";s:2:"ru";s:13:"FORMAT_STRING";s:9:"# руб.";s:9:"FULL_NAME";s:10:"Рубль";s:9:"DEC_POINT";s:1:".";s:13:"THOUSANDS_SEP";s:0:"";s:8:"DECIMALS";s:1:"2";s:17:"THOUSANDS_VARIANT";s:1:"S";s:9:"HIDE_ZERO";s:1:"Y";s:10:"CREATED_BY";N;s:11:"DATE_CREATE";s:19:"2015-06-10 12:56:40";s:11:"MODIFIED_BY";N;s:11:"TIMESTAMP_X";s:19:"2015-06-10 12:56:40";}}}';
+return true;
+?>
