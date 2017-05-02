417bbaa6838cee848a33fffc63d44eb2ba30db4f
Oleg Kasatkin <oleg@lemon-art.ru>
2017-05-02 15:12:04 +0300

    upd gabarits vanna

--- tools/gabarits.php
+++ tools/gabarits.php
@@ -4,18 +4,18 @@ CModule::IncludeModule("iblock");
 
 $arFilter = Array(
  "IBLOCK_ID"=>12, 
- //"SECTION_ID" => 1308,
- //"INCLUDE_SUBSECTIONS" => "Y",
+ "SECTION_ID" => 1281,
+ "INCLUDE_SUBSECTIONS" => "Y",
 	"!PROPERTY_DLINA_SM" => false,
 	"!PROPERTY_SHIRINA_SM" => false,
-	"!PROPERTY_VYSOTA_SM" => false
+	"!PROPERTY_GLUBINA_MM" => false
  );
-$res = CIBlockElement::GetList(Array("SORT"=>"ASC"), $arFilter, Array("NAME", "ID", "PROPERTY_DLINA_SM", "PROPERTY_SHIRINA_SM", "PROPERTY_VYSOTA_SM", "PROPERTY_GABARITS"));
+$res = CIBlockElement::GetList(Array("SORT"=>"ASC"), $arFilter, Array("NAME", "ID", "PROPERTY_DLINA_SM", "PROPERTY_SHIRINA_SM", "PROPERTY_GLUBINA_MM", "PROPERTY_GABARITS"));
 while($ar_fields = $res->GetNext())
 {
 	$arSetProps = Array();
-	if ( $ar_fields["PROPERTY_DLINA_SM_VALUE"] && $ar_fields["PROPERTY_SHIRINA_SM_VALUE"] && $ar_fields["PROPERTY_VYSOTA_SM_VALUE"] ){
-		$gab = $ar_fields["PROPERTY_DLINA_SM_VALUE"] . "х" . $ar_fields["PROPERTY_SHIRINA_SM_VALUE"]. "х" . $ar_fields["PROPERTY_VYSOTA_SM_VALUE"];
+	if ( $ar_fields["PROPERTY_DLINA_SM_VALUE"] && $ar_fields["PROPERTY_SHIRINA_SM_VALUE"] && $ar_fields["PROPERTY_GLUBINA_MM_VALUE"] ){
+		$gab = $ar_fields["PROPERTY_DLINA_SM_VALUE"] . "х" . $ar_fields["PROPERTY_SHIRINA_SM_VALUE"]. "х" . $ar_fields["PROPERTY_GLUBINA_MM_VALUE"];
 		$arSetProps["GABARITS"] = $gab;
 		CIBlockElement::SetPropertyValuesEx($ar_fields["ID"], false, $arSetProps);
 	}
