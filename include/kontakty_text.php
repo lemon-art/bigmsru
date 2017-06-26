7d11f4c5fd46adb29318b11cf1c10128dde7dd33
Oleg Kasatkin <oleg@lemon-art.ru>
2017-06-26 12:02:28 +0300

    upd contacts

--- include/kontakty_text.php
+++ include/kontakty_text.php
@@ -28,9 +28,7 @@
 		<p>
 			 Мы работаем: Пн. - Сб.: 8:30 - 18:30 / Вс.: 9:00 - 17:00
 		</p>
-		<div class="feedback">
- <a class="modalbox btn" href="#feedback">написать нам</a>
-		</div>
+
 	</td>
 </tr>
 </tbody>
