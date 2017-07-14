d64784839aca27a296e322e63d4ce3d8479057b3
Oleg Kasatkin <oleg@lemon-art.ru>
2017-07-14 18:22:26 +0300

    metrica

--- bitrix/templates/bigms_new/footer.php
+++ bitrix/templates/bigms_new/footer.php
@@ -341,5 +341,37 @@
     </div>
 	
 	
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter31721621 = new Ya.Metrika({
+                    id:31721621,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+					params:window.yaParams||{ }
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/31721621" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+	
 </body>
 </html>
