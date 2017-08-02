02d43a8d903a832a98464e8a4a577f6ed1e369fc
Oleg Kasatkin <oleg@lemon-art.ru>
2017-08-02 08:49:08 +0300

    tools docs 2

--- tools/docs.php
+++ tools/docs.php
@@ -34,7 +34,7 @@ $APPLICATION->SetTitle("Документация");
 		"DISPLAY_PREVIEW_TEXT" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "24",
+		"IBLOCK_ID" => "23",
 		"IBLOCK_TYPE" => "sl",
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
 		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
