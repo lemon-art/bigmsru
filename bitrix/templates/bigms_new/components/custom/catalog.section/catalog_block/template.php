08636449fee83aa2ab83139afc55bf19e30ca82d
Oleg Kasatkin <oleg@lemon-art.ru>
2017-08-11 17:56:43 +0300

    upd section prev pict

--- bitrix/templates/bigms_new/components/custom/catalog.section/catalog_block/template.php
+++ bitrix/templates/bigms_new/components/custom/catalog.section/catalog_block/template.php
@@ -107,7 +107,7 @@ foreach ($arResult['ITEMS'] as $key => $arItem):
                         <a href="<?=$arItem['DETAIL_PAGE_URL']?>">
 							<?
 							//Уменьшаем картинку для баннера
-							$PICT = ('Y' == $arItem['SECOND_PICT'] ? $arItem['PREVIEW_PICTURE_SECOND'] : $arItem['PREVIEW_PICTURE']);
+							$PICT = ('Y' == $arItem['SECOND_PICT'] ? $arItem['PREVIEW_PICTURE'] : $arItem['PREVIEW_PICTURE']);
 							$file = CFile::ResizeImageGet($PICT['ID'], array('width'=>250, 'height'=>200), BX_RESIZE_IMAGE_PROPORTIONAL_ALT, true);
 							$PICT['SRC'] = $file['src'];
 							$PICT['WIDTH'] = $file['width'];
