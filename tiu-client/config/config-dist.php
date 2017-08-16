4896d8418b19db74476d7c0b2526a28f1a0fdafe
Oleg Kasatkin <oleg@lemon-art.ru>
2017-08-16 11:12:58 +0300

    upd tiu 2

--- tiu-client/config/config-dist.php
+++ tiu-client/config/config-dist.php
@@ -4,7 +4,7 @@ $config = array(
      * xml link (like https://my.tiu.ru/cabinet/export_orders/xml/1234567?hash_tag=472g8fzb1af38d35f2c521dc8a1e1208)
      * can be taken here: https://my.tiu.ru/cabinet/order/export_orders
      */
-    'tiu_xml_url' => '',
+    'tiu_xml_url' => 'https://my.tiu.ru/cabinet/export_orders/xml/2211171?hash_tag=d0b41774e7d41aad0425250eee4d0eca',
 
     'retailcrm_url' => 'https://bigms.retailcrm.ru',
     'retailcrm_apikey' => 'lEFlWKrChHcFIl6colcsCUD6gIVJd75H',
@@ -13,7 +13,7 @@ $config = array(
     /**
      * upload orders only from a certain date (Y-m-d H:i:s)
      */
-    'date_from' => '',
+    'date_from' => '2017-08-07 00:00:00',
 
     'order_prefix' => '', // optional
     'set_external_ids' => false, // set externalId field
