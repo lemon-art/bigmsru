b1f7e41e5d2f5775cfa6afaa897a10f86d933b91
Oleg Kasatkin <oleg@lemon-art.ru>
2018-04-12 08:05:25 +0300

    left menu

--- bitrix/templates/bigms_new/js/main.js
+++ bitrix/templates/bigms_new/js/main.js
@@ -148,7 +148,7 @@ $(document).ready(function() {
   var screenHeight = document.documentElement.clientHeight,
       headerHeight = $('.header').innerHeight(),
      // sidebarHeight = $('.sidebar').innerHeight(),
-	  sidebarHeight = 1100;
+	  sidebarHeight = 1200;
       ifToggle = $('body').hasClass('menu-fix') || false,
       menuTop = 0,
       dataClick = ($('.sidebar').data('click') == false)? false: true;
