4da17f71df990176949289141e613871031838b7
oleg <oleg@lemon-art.ru>
2018-04-18 00:09:20 +0300

    oplata

--- bitrix/templates/bigms_new/components/bitrix/sale.basket.basket/cart/basket_items.php
+++ bitrix/templates/bigms_new/components/bitrix/sale.basket.basket/cart/basket_items.php
@@ -350,6 +350,7 @@ if ($countPropduct > 0):
                             <li class="cart-payment__item cart-payment__item_cash">Наличные</li>
                             <li class="cart-payment__item cart-payment__item_card">Банковские карты</li>
                             <li class="cart-payment__item cart-payment__item_transfer">Банковский перевод</li>
+							<li class="cart-payment__item cart-payment__item_credit">В кредит</li>
                             <li class="cart-payment__item cart-payment__item_sber">Сбербанк Онлайн</li>
                             <li class="cart-payment__item cart-payment__item_alfa">Альфа клик</li>
                             <li class="cart-payment__item cart-payment__item_vtb">ВТБ24-Онлайн</li>
