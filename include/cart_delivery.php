612e6899fb96c95cd961a181a5002b29b69f969a
oleg <oleg@lemon-art.ru>
2018-04-25 10:40:03 +0300

    cart info

--- include/cart_delivery.php
+++ include/cart_delivery.php
@@ -1,37 +1,59 @@
 <div class="col-lg-11 col-md-11 col-sm-11">
-                          <strong class="cart-delivery__title">Инженерная сантехника</strong>
+                          <strong class="cart-delivery__title">Доставка Автотранспортом</strong>
                           <strong class="cart-delivery__subtitle">В пределах МКАД:</strong>
                           <ul class="cart-delivery__list">
-                            <li class="cart-delivery__item">До 15 000 рублей - 500 руб.</li>
-                            <li class="cart-delivery__item">Свыше 15 000 руб. - БЕСПЛАТНО</li>
+                            <li class="cart-delivery__item">До 10 000 рублей - 350 руб.</li>
+                            <li class="cart-delivery__item">Свыше 10 000 руб. – БЕСПЛАТНО, за исключением крупногабаритных товаров 
+(Водонагреватели, Котлы, Насосы, Ванны, Унитазы, Инсталяции, Душевые системы, Полотенцесушители, Радиаторы).</li>
                           </ul>
+						  <div class="row cart-delivery__row">
                           <strong class="cart-delivery__subtitle">За пределы МКАД:</strong>
-                          <p class="cart-delivery__text">Тариф по Мск + 40 руб./1км.</p>
-                        </div>
+                          <p class="cart-delivery__text">350 руб + 30 руб. за каждый 1 км от МКАДа</p>
+                         </div>
+						 <div class="row cart-delivery__row">
+							<strong class="cart-delivery__subtitle">По МО:</strong>
+							 <ul class="cart-delivery__list">
+								<li class="cart-delivery__item">до 1 км от МКАД – 500 руб.</li>
+								<li class="cart-delivery__item">от 1 до 6 км от МКАД - 600 руб.</li>
+								<li class="cart-delivery__item">от 1 до 6 км от МКАД - 600 руб.</li>
+							  </ul>
+						 </div>
+					   </div>
+					   
+					   
                         <div class="col-lg-13 col-md-13 col-sm-13">
-                          <strong class="cart-delivery__title">Бытовая сантехника</strong>
+                          <strong class="cart-delivery__title">Доставка по регионам России, Казахстана при 100% предоплате</strong>
                           <div class="row cart-delivery__row">
-                            <div class="col-lg-10 col-md-10 col-sm-10">
-                              <strong class="cart-delivery__subtitle">По Москве:</strong>
-                              <p class="cart-delivery__text">500-1000 руб.</p>
-                            </div>
-                            <div class="col-lg-20 col-md-20 col-sm-20">
-                              <strong class="cart-delivery__subtitle">В регионы:</strong>
-                              <p class="cart-delivery__text">По тарифам транспортной компании.</p>
-                            </div>
+                            <strong class="cart-delivery__subtitle">Транспортной компанией:</strong>
+                          <ul class="cart-delivery__list">
+                            <li class="cart-delivery__item">малогабаритного товара до Транспортной компании - 300 руб.</li>
+                            <li class="cart-delivery__item">крупногабаритного товара до Транспортной компании - 500 руб.</li>
+							<li class="cart-delivery__item"> на сумму свыше 10000 руб.  - БЕСПЛАТНО, кроме крупногабаритного товара:
+(Водонагреватели, Котлы, Насосы, Ванны, Унитазы, Инсталяции, Душевые системы, Полотенцесушители, Радиаторы).
+</li>
+						  </ul>
                           </div>
                           <div class="row cart-delivery__row">
-                            <div class="col-lg-10 col-md-10 col-sm-10">
-                              <strong class="cart-delivery__subtitle">За МКАД:</strong>
-                              <p class="cart-delivery__text">+ 40 руб. за 1 км.</p>
-                            </div>
-                            <div class="col-lg-20 col-md-20 col-sm-20">
-                              <strong class="cart-delivery__subtitle">Подвоз до терминала:</strong>
-                              <p class="cart-delivery__text">По тарифам для Мск и МО.</p>
-                            </div>
+                            <strong class="cart-delivery__subtitle">Почтой России:</strong>
+                            <p class="cart-delivery__text">доставка  - 500 руб. до отделения в Вашем населенном пункте. Принимаются только малогабаритные посылки весом до 6 кг.
+</p>
                           </div>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6">
-                          <strong class="cart-delivery__title">Самовывоз</strong>
-                          <p class="cart-delivery__text">Время и место самовывоза согласуйте с менеджером по телефону</p>
-                        </div>
+							<div class="row cart-delivery__row">
+								<strong class="cart-delivery__subtitle">Самовывоз</strong>
+								<p class="cart-delivery__text">Оставляйте предварительно заявку на сайте, а время и место необходимо согласовать с менеджером по телефону
+</p>
+							</div>
+							<div class="row cart-delivery__row">
+								<strong class="cart-delivery__subtitle">КУРЬЕР (в пределах черты МКАД)</strong>
+								<p class="cart-delivery__text">Пешим курьером осуществляется доставка малогабаритных грузов весом до 6 кг с небольшим объемом.</p>
+								<br>
+									<ul class="cart-delivery__list">
+										<li class="cart-delivery__item">до 10000 руб –350 руб</li>
+										<li class="cart-delivery__item">свыше 10 000 руб – Бесплатно</li>
+									</ul>
+								
+							</div>
+							
+						</div>
