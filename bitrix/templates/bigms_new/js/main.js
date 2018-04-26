11300842e5c4b41380e6a8adcf4697b206e03d05
oleg <oleg@lemon-art.ru>
2018-04-27 00:50:15 +0300

    main page

--- bitrix/templates/bigms_new/js/main.js
+++ bitrix/templates/bigms_new/js/main.js
@@ -22,6 +22,20 @@ $(document).ready(function() {
     mainOwl.trigger('refresh.owl.carousel', []);
   });
   
+	$( "#main_tabs" ).tabs({
+      beforeLoad: function( event, ui ) {
+        ui.jqXHR.fail(function() {
+          ui.panel.html(
+            "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+            "If this wouldn't be a demo." );
+        });
+      }
+    });
+	
+	  $('.vertical_tabs li').click(function(e) {
+		$(this).find('a').click();
+	  });
+
   
 
 	
