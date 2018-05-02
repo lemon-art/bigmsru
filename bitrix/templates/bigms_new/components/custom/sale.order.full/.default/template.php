3f8c86dbbdde61b7c0caff5ea45e682d2f0ec42b
oleg <oleg@lemon-art.ru>
2018-05-02 15:54:01 +0300

    payment

--- bitrix/templates/bigms_new/components/custom/sale.order.full/.default/template.php
+++ bitrix/templates/bigms_new/components/custom/sale.order.full/.default/template.php
@@ -6,30 +6,62 @@
           <div class="content-ordered">
             <h1 class="title-h1 content-ordered__title">Ваш заказ <span class="title-h1_marked">№ <?=$arResult["ORDER_ID"]?></span> оформлен.</h1>
             <p class="content-ordered__text">В ближайшее время наш менеджер свяжется с вами<br>для уточнения деталей оплаты и доставки.</p>
-            <p class="content-ordered__text">Спасибо, что выбрали нас.</p>
+            
             
 
 <?
-			if ($arResult["PAY_SYSTEM"]["ID"] == 7)
+			if (!empty($arResult["PAY_SYSTEM"]))
 			{
 				?>
 				<p class="content-ordered__text">
 							<?echo GetMessage("STOF_ORDER_PAY_ACTION1")?> <?= $arResult["PAY_SYSTEM"]["NAME"] ?>
 				</p>
 				
-				<?$APPLICATION->IncludeComponent(
-					"bitrix:sale.order.payment.receive",
-					"",
-					Array(
-						"PAY_SYSTEM_ID_NEW" => $arResult["PAY_SYSTEM"]["ID"]
-					)
-				);?>
 				
-			<?
+				
+
+					<?
+					if (strlen($arResult["PAY_SYSTEM"]["ACTION_FILE"]) > 0)
+					{
+						?>
+								<?
+								if ($arResult["PAY_SYSTEM"]["NEW_WINDOW"] == "Y")
+								{
+									?>
+									<script language="JavaScript">
+										window.open('<?=$arParams["PATH_TO_PAYMENT"]?>?ORDER_ID=<?=urlencode(urlencode($arResult["ORDER"]["ACCOUNT_NUMBER"]))?>');
+									</script>
+									<?= str_replace("#LINK#", $arParams["PATH_TO_PAYMENT"]."?ORDER_ID=".urlencode(urlencode($arResult["ORDER"]["ACCOUNT_NUMBER"])), GetMessage("STOF_ORDER_PAY_WIN")) ?>
+									<?
+								}
+								else
+								{
+									if (strlen($arResult["PAY_SYSTEM"]["PATH_TO_ACTION"])>0)
+									{
+										try
+										{
+											include($arResult["PAY_SYSTEM"]["PATH_TO_ACTION"]);
+										}
+										catch(\Bitrix\Main\SystemException $e)
+										{
+											if($e->getCode() == CSalePaySystemAction::GET_PARAM_VALUE)
+												$message = GetMessage("SOA_TEMPL_ORDER_PS_ERROR");
+											else
+												$message = $e->getMessage();
+
+											echo '<span style="color:red;">'.$message.'</span>';
+										}
+									}
+								}
+								?>
+						<?
+					}
+					?>
+				<?
 			}
 			?>
 			
-			
+			<p class="content-ordered__text">Спасибо, что выбрали нас.</p>
 			
 			<a href="/" class="content-cart__back content-ordered__back">Вернуться на главную</a>
 			
