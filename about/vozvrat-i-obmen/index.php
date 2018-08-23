b11867eef1bff8bd46ea6ef934a87623f5b2afe0
Oleg Kasatkin <oleg@lemon-art.ru>
2018-08-23 08:12:33 +0300

    about

--- about/vozvrat-i-obmen/index.php
+++ about/vozvrat-i-obmen/index.php
@@ -9,7 +9,13 @@ $APPLICATION->SetTitle("Гарантия и возврат");
 	<div class="row">
 		<div class="col-lg-20 col-md-20 col-sm-20 content-about__main content-guarantee">
 			<div class="content-guarantee__row">
- <strong class="content-guarantee__title">Во избежание недоразумений</strong>
+ <strong class="content-guarantee__title">
+				<p>
+					 Гарантия
+				</p>
+ </strong><strong class="content-guarantee__title">Все вопросы, связанные с гарантийным ремонтом товара, с ремонтом товара, поставкой запчастей – решаются с авторизованными сервисными центрами, адреса и телефоны которых указаны в гарантийных талонах на товар или на сайте производителя товара.</strong><strong class="content-guarantee__title"><br>
+				<br>
+				 Во избежание недоразумений</strong>
 				<ul class="content-guarantee__list">
 					<li class="content-guarantee__item">В момент получения товара от курьера или на складе магазина убедитесь в отсутствии дефектов и полноте комплектации заказа.</li>
 					<li class="content-guarantee__item">При обнаружении недостатков, не подписывайте акт приема-передачи, а свяжитесь с менеджером для прояснения ситуации и согласования дальнейших взаимных действий.</li>
@@ -19,7 +25,8 @@ $APPLICATION->SetTitle("Гарантия и возврат");
 				</p>
 			</div>
 			<div class="content-guarantee__row">
- <strong class="content-guarantee__title">Условия возврата товара</strong>
+ <strong class="content-guarantee__title"><br>
+				Условия возврата товара</strong>
 				<p class="content-guarantee__text">
 					 Купленный товар можно вернуть в срок до двух месяцев при соблюдении следующих условий:
 				</p>
@@ -33,7 +40,7 @@ $APPLICATION->SetTitle("Гарантия и возврат");
 				</p>
 			</div>
 			<div class="content-guarantee__row">
-				<br>
+ <br>
 			</div>
 		</div>
 		<div class="col-lg-6 col-lg-offset-4 col-md-6 col-md-offset-4 col-sm-6 col-sm-offset-4 content-about__nav about-nav">
@@ -61,4 +68,4 @@ Array(
 		</div>
 	</div>
 </div>
-<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
