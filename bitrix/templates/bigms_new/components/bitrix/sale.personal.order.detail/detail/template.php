1c993fa18372c15cb81bc7cc8c082f956568623e
oleg <oleg@lemon-art.ru>
2018-12-17 14:17:57 +0300

    detail order

--- bitrix/templates/bigms_new/components/bitrix/sale.personal.order.detail/detail/template.php
+++ bitrix/templates/bigms_new/components/bitrix/sale.personal.order.detail/detail/template.php
@@ -313,9 +313,6 @@ $APPLICATION->AddChainItem(GetMessage('SPOD_ORDER') . ' ' . GetMessage('SPOD_NUM
 						</td>
 						<td>
 						
-						<pre>
-						<?print_r( $arResult['PAYMENT'] );?>
-						</pre>
 						
 											<?
 											foreach ($arResult['PAYMENT'] as $payment)
